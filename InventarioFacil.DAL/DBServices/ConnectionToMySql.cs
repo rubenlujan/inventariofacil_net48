ba51c072ba4ff7029f2ca4444709fe69445cc862
PC-RLO\RubenL <lujanortegaruben@gmail.com>
2023-06-12 20:42:29 -0600

    correccion login

--- InventarioFacil.DAL/DBServices/ConnectionToMySql.cs
+++ InventarioFacil.DAL/DBServices/ConnectionToMySql.cs
@@ -1,5 +1,6 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
+using System.Buffers.Text;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
@@ -17,11 +18,17 @@ namespace InventarioFacil.DAL.DBServices
         {
             //Establecer la cadena de conexión.
             connectionString = ConfigurationManager.ConnectionStrings["mySql"].ToString();
+            var uidBytes = Convert.FromBase64String(ConfigurationManager.AppSettings["uid"].ToString());
+            var uid = "uid = " + Encoding.UTF8.GetString(uidBytes);
+            var pidBytes = Convert.FromBase64String(ConfigurationManager.AppSettings["pid"].ToString());
+            var pid = "pwd = " + Encoding.UTF8.GetString(pidBytes);
+            connectionString += uid + ";" + pid + ";";
         }
         protected MySqlConnection GetConnection()
         {
             //Este métedo se encarga de establecer y devolver el objeto de conexión a SQL Server.
             return new MySqlConnection(connectionString);
         }
+
     }
 }
